1188829832359a073559c0a3e639d4a2637c2275
David Muniz <davidsmuniz08@gmail.com>
2022-09-21 11:11:21 -0300

    adcionando o campo idade

--- projetoJava/src/curso/java/david/Aula71Git.java
+++ projetoJava/src/curso/java/david/Aula71Git.java
@@ -5,6 +5,16 @@ public class Aula71Git {
 	 private String nome;
 	 
 	 private String sobrenome;
+	 
+	 private int idade;
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
 
 	public String getNome() {
 		return nome;
